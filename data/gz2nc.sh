d7ada2263cafcb031a2fd5e5f5ba13c8902c99f7
Andrew Wood <amwood2002@gmail.com>
2022-03-31 17:15:32 -0400

    sh downloads

---
+++ data/gz2nc.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+# 
+# gz to grid converter
+#
+# Creator: Andrew Wood
+# Date Created: 3/31/22
+# Last Modified: 3/31/22 
+
+# Inputs
+# $1 folder of compressed spherical harmonic coeff files
+
+path="$1/"
+
+
